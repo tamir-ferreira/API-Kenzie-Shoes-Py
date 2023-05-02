59de34029b03ca22ab921862e72dadbbef901f59
Andrew <andrewairamdasilva@gmail.com>
2023-05-02 15:55:53 -0300

    feat: project scaffold and models added

---
+++ products/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Category(models.TextChoices):
+    BOTAS = "Botas",
+    CHINELOS_SANDÁLIAS = "Chinelos e Sandálias",
+    CHUTEIRAS = "Chuteiras",
+    SAPATÊNIS = "Sapatênis",
+    TÊNIS = "Tênis",
+    TÊNIS_CORRIDA = "Tênis de corrida",
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    value = models.DecimalField(max_digits=12, decimal_places=2)
+    category = models.CharField(max_length=20, choices=Category.choices,
+                                default=Category.BOTAS)
+    quantities = models.IntegerField(default=0)
+    bio = models.TextField()
+    image_product = models.URLField(max_length=200)
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="user_product",
+    )
+
+    cart = models.ManyToManyField("cart.Cart", related_name="cart")
---
+++ users/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=127, unique=True)
+    is_seller = models.BooleanField(default=False, null=True, blank=True)
+    bio = models.TextField()
+    image_user = models.URLField(max_length=200)
+
+    address = models.ForeignKey(
+        "addresses.Address",
+        on_delete=models.CASCADE,
+        related_name="address_user",
+    )
+
+
+class OrderStatus(models.TextChoices):
+    REALIZADO = "Pedido realizado"
+    ANDAMENTO = "Pedido em andamento"
+    ENTREGUE = "Pedido entregue"
+
+
+class UserOrder(models.Model):
+    status = models.CharField(max_length=20, choices=OrderStatus.choices,
+                              default=OrderStatus.ANDAMENTO)
+    buyed_at = models.TimeField(auto_now_add=True)
+
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="user_order"
+    )
