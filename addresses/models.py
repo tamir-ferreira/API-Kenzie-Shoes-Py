1978b160aa793cf3dcb5a60a111d981aef37ffc9
Andrew <andrewairamdasilva@gmail.com>
2023-05-02 17:28:20 -0300

    fix: changed buyed_at in userorder to datetimefield and zipcode in addresses to charfield

--- addresses/models.py
+++ addresses/models.py
@@ -5,6 +5,6 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     number = models.IntegerField()
     add_on = models.CharField(max_length=5, blank=True, null=True)
-    zipcode = models.IntegerField()
+    zipcode = models.CharField(max_length=10)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=2)
