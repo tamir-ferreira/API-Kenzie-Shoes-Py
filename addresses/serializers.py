69dd50058d0acdec752cebf92ff818553f6ae5e6
Tamir S. Ferreira <bigtam_rj@yahoo.com.br>
2023-05-03 18:31:26 -0300

    feat: create address ok

--- addresses/serializers.py
+++ addresses/serializers.py
@@ -6,9 +6,3 @@ class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = "__all__"
-
-    def create(self, validated_data):
-        print("=" * 150)
-        print(validated_data)
-
-        return Address.objects.create(**validated_data)
--- users/serializers.py
+++ users/serializers.py
@@ -1,29 +1,24 @@
-from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from addresses.models import Address
 from addresses.serializers import AddressSerializer
+from rest_framework import serializers
+from addresses.models import Address
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    # address = serializers.PrimaryKeyRelatedField(read_only=True)
-    address_id = serializers.IntegerField(source="address.id")
+    address = AddressSerializer()
 
     email = serializers.EmailField(
         validators=[UniqueValidator(queryset=User.objects.all())],
     )
 
     def create(self, validated_data: dict) -> User:
-        print("=" * 150)
-        print(validated_data)
         address_create = validated_data.pop("address")
-        user = User.objects.create_user(**validated_data)
-        Address.objects.create(user=user, **address_create)
-
-        # return User.objects.create_superuser(**validated_data)
+        address = Address.objects.create(**address_create)
+        user = User.objects.create_user(address=address, **validated_data)
         return user
 
-    def update(self, instance: User, validated_data: dict) -> User:
+    """def update(self, instance: User, validated_data: dict) -> User:
         password = validated_data.pop("password", None)
         for key, value in validated_data.items():
             setattr(instance, key, value)
@@ -33,6 +28,7 @@ class UserSerializer(serializers.ModelSerializer):
         instance.save()
 
         return instance
+    """
 
     class Meta:
         model = User
@@ -46,7 +42,7 @@ class UserSerializer(serializers.ModelSerializer):
             "is_superuser",
             "is_seller",
             "image_user",
-            "address_id",
+            "address",
         ]
         read_only_fields = ["is_superuser"]
         extra_kwargs = {"password": {"write_only": True}}
