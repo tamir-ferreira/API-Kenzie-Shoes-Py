853eee8c62c88bbcc5b6fd48ea79db37286a4f6f
camila de souza <camila.sparquitetura@gmail.com>
2023-05-04 14:37:37 -0300

    fix/line 23 urls.py from core

--- _core/urls.py
+++ _core/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("cart.urls")),
+    path("api/", include("products.urls")),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+class ProductPaginator(PageNumberPagination):
+    page_size = 10
+
+
+class ProductView(generics.ListCreateAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    paginator_class = ProductPaginator
+
+    def perform_create(self, serializer) -> None:
+        serializer.save(user=self.request.user)
+
+
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = "pk"
