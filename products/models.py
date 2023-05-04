af2b713de796a44bbb43c259c9bdee924fb6789f
Lilian Fernandes Oliveira Dias <biologia.lilianfernandes@gmail.com>
2023-05-04 15:56:14 -0300

    feat:productDetail and tests

--- products/models.py
+++ products/models.py
@@ -2,19 +2,20 @@ from django.db import models
 
 
 class Category(models.TextChoices):
-    BOTAS = "Botas",
-    CHINELOS_SANDALIAS = "Chinelos e Sandálias",
-    CHUTEIRAS = "Chuteiras",
-    SAPATENIS = "Sapatênis",
-    TENIS = "Tênis",
-    TENIS_CORRIDA = "Tênis de corrida",
+    BOTAS = ("Botas",)
+    CHINELOS_SANDALIAS = ("Chinelos e Sandálias",)
+    CHUTEIRAS = ("Chuteiras",)
+    SAPATENIS = ("Sapatênis",)
+    TENIS = ("Tênis",)
+    TENIS_CORRIDA = ("Tênis de corrida",)
 
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
     value = models.DecimalField(max_digits=12, decimal_places=2)
-    category = models.CharField(max_length=20, choices=Category.choices,
-                                default=Category.BOTAS)
+    category = models.CharField(
+        max_length=20, choices=Category.choices, default=Category.BOTAS
+    )
     quantities = models.IntegerField(default=0)
     bio = models.TextField()
     image_product = models.URLField(max_length=200)
